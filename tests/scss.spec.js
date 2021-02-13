322a721c79d9ab9da811b9a3a12e53f417b4a794
Nicholas Cornish <Nicholas.Cornish@de.bosch.com>
2021-02-14 00:05:08 +0100

    function refactoring, update tests, bump version

--- tests/scss.spec.js
+++ tests/scss.spec.js
@@ -5,7 +5,7 @@
 var path = require('path');
 var sassTrue = require('sass-true');
 
-sassTrue.runSass({ file: path.join(__dirname, 'power.spec.scss') }, { describe, it });
+sassTrue.runSass({ file: path.join(__dirname, 'functions.spec.scss') }, { describe, it });
 sassTrue.runSass({ file: path.join(__dirname, 'scales.spec.scss') }, { describe, it });
 sassTrue.runSass({ file: path.join(__dirname, 'default.spec.scss') }, { describe, it });
 sassTrue.runSass({ file: path.join(__dirname, 'rounded.spec.scss') }, { describe, it });
